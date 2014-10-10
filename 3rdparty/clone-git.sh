63cc830fe6daf003002e1fed50a56fcfed8284af
Jan Winkler <winkler@cs.uni-bremen.de>
2014-10-10 08:11:59 +0200

    Fixed a build bug that would trigger pulling of 3rdparty code every time
    Even if the code shouldn't be rebuilt (i.e. when the `built_flat' file is present), the repository for 3rdparty/json-c code would still be updated. This isn't happening anymore now.

--- 3rdparty/clone-git.sh
+++ 3rdparty/clone-git.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-if [ ! -d $2 ]; then
-    git clone $1 $2
-else
-    cd $2
-    git pull
-    cd -
+if [ ! -e "$2/built_flag" ]; then
+    if [ ! -d $2 ]; then
+	git clone $1 $2
+    else
+	cd $2
+	git pull
+	cd -
+    fi
 fi
